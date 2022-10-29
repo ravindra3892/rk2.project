9ea9947f3ada11f4e85120ee6953ae9ffaaa8115
ravindra3892 <107902233+ravindra3892@users.noreply.github.com>
2022-10-29 08:55:29 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
 
-    System.out.println("happy coming siva ratri to world and kartikamasam ");
+    System.out.println("happy coming siva ratri to world and kartikamasam... ");
 
   }
 }
