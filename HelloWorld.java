d99c77d30bdb74d1276e6ed3e79ce7125d582a47
ravindra3892 <ravindra3892@gmail.com>
2022-10-29 11:58:22 +0000

    hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
 
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
-    System.out.println("Hello world!");
+    System.out.println("happy coming siva ratri to all world ");
+
   }
 }
